53dd57e5df3b3ad0900519272fd5723541383196
P Niranjan Roy <niranjan275865@gmail.com>
2026-04-02 08:40:28 +0530

    Add case- insensitive string comparison

---
+++ strings/string_compare_ignore_case.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+ 
+ strcat(): it means string concatenation. it is useful to combine two strings
+ 
+ ******************************************************************************/
+ 
+ #include<stdio.h>
+ #include<conio.h>
+ #include<string.h>
+ int main()
+ {
+     char a[20],b[20];
+     clrscr();
+     printf("Type first name:");
+     gets(a);
+     
+     printf("Type last name:");
+     gets(b);
+     
+     if(strcmpi(a,b)==0)
+     printf("Equal");
+     else
+     printf("Not equal");
+     
+     getch();
+ }
